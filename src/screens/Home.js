eaca727fd6856d08b0c82270c4386a647a21053e
DavidVallecios <00078217@uca.edu.sv>
2021-09-30 14:13:03 -0600

    Corrección de diseño en la pantalla de Home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,145 +1,160 @@
 import { map } from 'lodash'
-import React, { Component, useState } from 'react'
-import { View } from 'react-native'
-import { StyleSheet, SafeAreaView } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, SafeAreaView, View } from 'react-native'
 import { Text } from 'react-native-elements'
 import { ScrollView } from 'react-native-gesture-handler'
-import { Searchbar } from 'react-native-paper'
-import { IconButton} from 'react-native-paper'
-
-
+import { Searchbar, IconButton } from 'react-native-paper'
 
 const Home = ({ navigation }) => {
 	const categories = [
 		{
 			id: 1,
-			name: "Reciente",
+			name: 'Reciente',
 		},
 		{
 			id: 2,
-			name: "Hogar",
+			name: 'Hogar',
 		},
 		{
 			id: 3,
-			name: "Belleza",
+			name: 'Belleza',
 		},
 		{
 			id: 4,
-			name: "Ropa",
+			name: 'Ropa',
 		},
 		{
 			id: 5,
-			name: "Telefonos",
+			name: 'Telefonos',
 		},
 		{
 			id: 6,
-			name: "Computadoras",
+			name: 'Computadoras',
 		},
-		];
-	
-		
-	const [categorySelected, setCategorySelected] = useState(1);
-	
-	const onChangeCategory = (category) => {
-		setCategorySelected(category);
-	};
-	
-	
+	]
 
+	const [categorySelected, setCategorySelected] = useState(1)
 
+	const onChangeCategory = category => {
+		setCategorySelected(category)
+	}
 
 	return (
-		<SafeAreaView>
-			
-			<IconButton raised icon="bell" color="#003C95" style={styles.notification}/>
-			<Searchbar style={styles.input} ></Searchbar>
-			
+		<SafeAreaView style={styles.container}>
+			<View style={styles.notificationContainer}>
+				<Text style={styles.notificationText}>10</Text>
+				<IconButton
+					raised
+					icon="bell"
+					color="#003C95"
+					style={styles.notification}
+				/>
+			</View>
+			<Searchbar style={styles.input} />
+
 			<View style={styles.categories}>
 				<ScrollView
 					horizontal
 					showsHorizontalScrollIndicator={false}
 					style={styles.categoryList}>
-					{map(categories,(catg)=>(
-						<Text key={catg.id}
-						style={[
-							catg.id !== categorySelected ? styles.category : styles.categorySelec,
-						]}
-					onPress={()=> onChangeCategory(catg.id)}>
-					{catg.name}
-					</Text>
+					{map(categories, catg => (
+						<Text
+							key={catg.id}
+							style={[
+								styles.category,
+								categorySelected === catg.id && styles.categorySelec,
+							]}
+							onPress={() => onChangeCategory(catg.id)}>
+							{catg.name}
+						</Text>
 					))}
-
 				</ScrollView>
 			</View>
-
 		</SafeAreaView>
-		
-		
 	)
 }
-
 
 export default Home
 
 const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#fff',
+		height: '100%',
+	},
 	input: {
-		height: 50,
-		width: '95%',
+		height: 40,
+		width: '90%',
 		borderRadius: 10,
 		fontSize: 18,
 		alignSelf: 'center',
-
+	},
+	notificationContainer: {
+		position: 'relative',
+		width: 60,
+		height: 60,
+		alignSelf: 'flex-end',
+		marginBottom: 40,
+		marginRight: 20,
+		marginTop: 20,
 	},
 	notification: {
 		height: 50,
 		color: '#fff',
-		width: '15%',
+		width: 50,
 		borderRadius: 10,
 		fontSize: 18,
-		borderWidth: 0.5,
-		alignSelf: 'flex-end',
-		marginBottom: 40,
-		backgroundColor: "#fff"
+		backgroundColor: '#fff',
+		borderColor: '#fff',
+		shadowColor: '#000',
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 3.84,
+		elevation: 5,
+	},
+	notificationText: {
+		position: 'absolute',
+		borderRadius: 100,
+		backgroundColor: '#060948',
+		fontWeight: 'bold',
+		color: '#fff',
+		height: 28,
+		width: 28,
+		textAlign: 'center',
+		textAlignVertical: 'center',
+		elevation: 6,
+		right: -10,
+		top: -10,
+	},
+	categories: {
+		marginTop: 20,
+		marginBottom: 50,
 	},
 
-	
-	shadowOffset: {
-	width: 0,
-	height: 2,
-},
-
-categories: {
-    marginTop: 20,
-    marginBottom: 50,
-},
-
 	categoryList: {
-    marginTop: 5,
-    marginBottom: 15,
-    paddingHorizontal: 20,
-    padding: 10,
-},
-category: {
-    marginRight: 20,
-    fontSize: 16,
-	borderWidth: 0.5,
-	padding: 10,
-	borderRadius: 10,
-	fontFamily: "Roboto",
-	fontWeight: "bold",
-	backgroundColor: "#fff"
-},
-
-categorySelec: {
-    marginRight: 20,
-    fontSize: 16,
-	borderWidth: 0.5,
-	padding: 10,
-	borderRadius: 10,
-	fontFamily: "Roboto",
-	fontWeight: "bold",
-	backgroundColor: "#003C95",
-	color: "#fff"
-},
-
-});
+		marginTop: 5,
+		marginBottom: 15,
+		marginHorizontal: 20,
+	},
+	category: {
+		marginHorizontal: 5,
+		fontSize: 16,
+		borderWidth: 1,
+		paddingHorizontal: 10,
+		paddingVertical: 5,
+		borderRadius: 10,
+		fontFamily: 'Roboto',
+		fontWeight: 'bold',
+		backgroundColor: '#fff',
+		borderColor: '#E6E6E6',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	categorySelec: {
+		backgroundColor: '#003C95',
+		color: '#fff',
+	},
+})
