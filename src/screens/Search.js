73c1bdc7fda78905b0ea040c3a3ea1f70e4bb1ca
Oscar Lovato <00127417@uca.edu.sv>
2021-09-30 23:08:05 -0600

    search bar

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,14 +1,60 @@
 import React from 'react'
-import { StyleSheet, Text, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, SafeAreaView, View } from 'react-native'
+import { Searchbar } from 'react-native-paper'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
-const Search = ({Navigation}) => {
+const Search = ({navigation}) => {
     return (
         <SafeAreaView>
-            <Text>Search</Text>
+            <View style={styles.header}>
+                <Icon
+					name="arrow-back-ios"
+					size={30}
+					color="#003C95"
+					style={styles.backArrow}
+					onPress={() => navigation.goBack()}
+				/>
+                <Searchbar style={styles.search} ></Searchbar>
+            </View>
+
+            
+            
+            
         </SafeAreaView>
     )
 }
 
 export default Search
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    header: {
+        
+		paddingVertical: 10,
+		flexDirection: 'row',
+		alignItems: 'flex-start',
+		justifyContent: 'flex-start',
+	},
+
+    backArrow: {
+        
+        marginTop:5,
+		marginHorizontal: 10,
+	},
+
+    search: {
+		height: 50,
+		width: '70%',
+		borderRadius: 10,
+		fontSize: 18,
+		alignSelf: 'center',
+		marginTop: 5,
+		shadowColor: '#000',
+		shadowOffset: {
+		    width: 0,
+		    height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 3.84,
+		elevation: 5,
+	},
+})
